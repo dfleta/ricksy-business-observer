a9798cbbdb1246e0b4af23fde3f3f6cda501b526
dfleta <gelpiorama@gmail.com>
2020-02-18 01:51:05 +0100

    test(CreditCardTest): Testear pay() y credit() de CreditCard

--- src/test/java/ricksy/business/CreditCardTest.java
+++ src/test/java/ricksy/business/CreditCardTest.java
@@ -3,11 +3,31 @@ package ricksy.business;
 import org.junit.Test;
 import static org.junit.Assert.*;
 
+import org.junit.Before;
+
 public class CreditCardTest {
-    @Test public void constructortest() {
-        
-        CreditCard card = new CreditCard("Abradolf Lincler", "4916119711304546");
+
+    private CreditCard card = null;
+
+    @Before
+    public void setupCard() {
+        card = new CreditCard("Abradolf Lincler", "4916119711304546");
         assertNotNull("CreditCard creada", card);
+    }
+    @Test public void constructortest() {  
+        assertNotNull("CreditCard creada", card);      
         assertEquals("4916119711304546", card.number());
+    }
+
+    @Test
+    public void payTestOK() {
+        card.pay(3000);
+        assertEquals(0, card.credit(), 0);
+    }
+
+    @Test
+    public void payTestNOTOK() {
+        card.pay(4000);
+        assertEquals(3000, card.credit(), 0);
     }
 }
