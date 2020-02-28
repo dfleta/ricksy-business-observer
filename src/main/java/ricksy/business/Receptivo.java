15ba6944a7486b3ed7d9486fdfbecae95a216357
dfleta <gelpiorama@gmail.com>
2020-02-28 20:38:28 +0100

    refactor(Receptivo): Reducir visibilidad Receptivo

--- src/main/java/ricksy/business/Receptivo.java
+++ src/main/java/ricksy/business/Receptivo.java
@@ -3,15 +3,15 @@ package ricksy.business;
 import java.util.LinkedHashSet;
 import java.util.Set;
 
-public class Receptivo {
+class Receptivo {
     
     private Set<GuestDispatcher> observers = new LinkedHashSet<>();
 
-    public void registra(GuestDispatcher observer) {
+    void registra(GuestDispatcher observer) {
         observers.add(observer);
     }
 
-    public void dispatch(CreditCard card) {
+    void dispatch(CreditCard card) {
         for (GuestDispatcher observer: observers) {
             observer.dispatch(card);
         }
